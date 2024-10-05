5fd40523f31e08f8b46162a161e867b84b9baf96
reamkf <68076678+reamkf@users.noreply.github.com>
2024-10-05 23:02:43 +0900

    update

--- table.js
+++ table.js
@@ -1,33 +1,13 @@
+// @ https://cdn.jsdelivr.net/gh/reamkf/nanoda-wiki@main/table.js
+
 document.addEventListener('DOMContentLoaded', (event) => {
 	main();
 });
-
 
 // =====================================================================================
 //  tablesorter
 //  URL: https://cdnjs.com/libraries/jquery.tablesorter
 // =====================================================================================
-function loadScript(src, attributes = {}) {
-    return new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.src = src;
-
-        // Set custom attributes
-        for (const [key, value] of Object.entries(attributes)) {
-            script.setAttribute(key, value);
-        }
-
-        script.onload = resolve;
-        script.onerror = reject;
-        document.head.appendChild(script);
-    });
-}
-
-function addCSS(css){
-	const style = document.createElement("style");
-	style.innerHTML = css;
-	document.head.append(style);
-}
 
 async function loadTableSorterScripts() {
     const scripts = [
