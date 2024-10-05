9de90a42eca844ce02ba8d2f04315b1a0f617020
reamkf <68076678+reamkf@users.noreply.github.com>
2024-10-05 23:14:19 +0900

    update

--- inject.js
+++ inject.js
@@ -2,10 +2,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	main();
 });
 
+/**
+ * 指定されたソースからスクリプトを読み込み、成功時と失敗時にそれぞれ解決または拒否されるPromiseを返します。
+ * @param {string} src - スクリプトのソースURL。
+ * @param {Object} attributes - スクリプト要素に追加する任意の属性。
+ * @returns {Promise<void>} スクリプトの読み込みの完了を示すPromise。
+ */
 function loadScript(src, attributes = {}) {
     return new Promise((resolve, reject) => {
         const script = document.createElement('script');
         script.src = src;
+
+		// デフォルトでcrossorigin属性を設定
+        if (!attributes.hasOwnProperty('crossorigin')) {
+            script.crossOrigin = 'anonymous';
+        }
 
         // Set custom attributes
         for (const [key, value] of Object.entries(attributes)) {
