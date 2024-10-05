f87afd5df26d47703e23a8da5a3d2ab930b8d1ca
reamkf <68076678+reamkf@users.noreply.github.com>
2024-10-06 00:43:53 +0900

    update

--- inject.js
+++ inject.js
@@ -1,45 +1,47 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-	main();
-});
+(() => {
+	document.addEventListener('DOMContentLoaded', (event) => {
+		main();
+	});
 
-/**
- * 指定されたソースからスクリプトを読み込み、成功時と失敗時にそれぞれ解決または拒否されるPromiseを返します。
- * @param {string} src - スクリプトのソースURL。
- * @param {Object} attributes - スクリプト要素に追加する任意の属性。
- * @returns {Promise<void>} スクリプトの読み込みの完了を示すPromise。
- */
-function loadScript(src, attributes = {}) {
-    return new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.src = src;
+	/**
+	* 指定されたソースからスクリプトを読み込み、成功時と失敗時にそれぞれ解決または拒否されるPromiseを返します。
+	* @param {string} src - スクリプトのソースURL。
+	* @param {Object} attributes - スクリプト要素に追加する任意の属性。
+	* @returns {Promise<void>} スクリプトの読み込みの完了を示すPromise。
+	*/
+	function loadScript(src, attributes = {}) {
+		return new Promise((resolve, reject) => {
+			const script = document.createElement('script');
+			script.src = src;
 
-        // デフォルトでcrossorigin属性を設定
-        if (!attributes.hasOwnProperty('crossorigin')) {
-            script.crossOrigin = 'anonymous';
-        }
+			// デフォルトでcrossorigin属性を設定
+			if (!attributes.hasOwnProperty('crossorigin')) {
+				script.crossOrigin = 'anonymous';
+			}
 
-        // Set custom attributes
-        for (const [key, value] of Object.entries(attributes)) {
-            script.setAttribute(key, value);
-        }
+			// Set custom attributes
+			for (const [key, value] of Object.entries(attributes)) {
+				script.setAttribute(key, value);
+			}
 
-        script.onload = resolve;
-        script.onerror = reject;
-        document.head.appendChild(script);
-    });
-}
+			script.onload = resolve;
+			script.onerror = reject;
+			document.head.appendChild(script);
+		});
+	}
 
-function addCSS(css){
-	const style = document.createElement("style");
-	style.innerHTML = css;
-	document.head.append(style);
-}
+	function addCSS(css){
+		const style = document.createElement("style");
+		style.innerHTML = css;
+		document.head.append(style);
+	}
 
 
-async function main() {
-    try {
-        await loadScript('https://cdn.jsdelivr.net/gh/reamkf/nanoda-wiki@0.0.7/table.js');
-    } catch (error) {
-        console.error("Error in main function:", error);
-    }
-}
+	async function main() {
+		try {
+			await loadScript('https://cdn.jsdelivr.net/gh/reamkf/nanoda-wiki@0.0.7/table.js');
+		} catch (error) {
+			console.error("Error in main function:", error);
+		}
+	}
+})();
