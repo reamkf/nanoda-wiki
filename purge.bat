d0434bae94eb1b1a1d75e46cd419c8db804d949e
reamkf <68076678+reamkf@users.noreply.github.com>
2024-10-06 13:37:00 +0900

    update

--- purge.bat
+++ purge.bat
@@ -1,2 +1,11 @@
-curl -X GET https://purge.jsdelivr.net/gh/reamkf/nanoda-wiki@main/inject.js
-curl -X GET https://purge.jsdelivr.net/gh/reamkf/nanoda-wiki@main/table.js
+@echo off
+setlocal
+
+if "%~1"=="" (
+	curl -X GET https://purge.jsdelivr.net/gh/reamkf/nanoda-wiki@main/inject.js
+	curl -X GET https://purge.jsdelivr.net/gh/reamkf/nanoda-wiki@main/table.js
+) else (
+	curl -X GET https://purge.jsdelivr.net/gh/reamkf/nanoda-wiki/%~1.js
+)
+
+endlocal
