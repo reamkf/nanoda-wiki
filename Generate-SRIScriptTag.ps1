1c3791a2d78c3f17e388fa3b8d210e0a06af929d
reamkf <68076678+reamkf@users.noreply.github.com>
2024-10-14 19:08:07 +0900

    Update Generate-SRIScriptTag.ps1 to automatically use latest commit hash

--- Generate-SRIScriptTag.ps1
+++ Generate-SRIScriptTag.ps1
@@ -1,7 +1,4 @@
 param(
-    [Parameter(Mandatory=$true)]
-    [string]$ScriptUrl,
-
     [Parameter(Mandatory=$false)]
     [string]$OutputFile = '.\inject.txt',
 
@@ -9,6 +6,21 @@ param(
     [ValidateSet("sha256", "sha384", "sha512")]
     [string]$HashAlgorithm = "sha384"
 )
+
+# Function to get the latest commit hash using git command
+function Get-LatestCommitHash {
+    try {
+        $latestHash = git rev-parse --short=7 HEAD
+        if ($LASTEXITCODE -ne 0) {
+            throw "Git command failed"
+        }
+        return $latestHash
+    }
+    catch {
+        Write-Error "Error fetching latest commit hash: $_"
+        exit 1
+    }
+}
 
 # Function to calculate SRI hash
 function Get-SRIHash {
@@ -34,12 +46,21 @@ function Get-SRIHash {
     }
 }
 
+# Get the latest commit hash
+$latestHash = Get-LatestCommitHash
+
+# Construct the CDN URL
+$repoUrl = "https://github.com/reamkf/nanoda-wiki"
+$cdnUrl = $repoUrl -replace "https://github.com/", "https://cdn.jsdelivr.net/gh/"
+$cdnUrl = $cdnUrl -replace "/$", ""
+$cdnUrl += "@$latestHash/nanoda-wiki.js"
+
 # Calculate SRI hash
-$sriHash = Get-SRIHash -Url $ScriptUrl -Algorithm $HashAlgorithm
+$sriHash = Get-SRIHash -Url $cdnUrl -Algorithm $HashAlgorithm
 
 # Generate script tag
 $scriptTag = @"
-<meta name="google-site-verification" content="q6H0qChDSXhbyT2OZJ2OMDZ_3VWAkE8Ccd-BRjHEEgU" /><script src="$ScriptUrl" integrity="$sriHash" crossorigin="anonymous"></script>>
+<meta name="google-site-verification" content="q6H0qChDSXhbyT2OZJ2OMDZ_3VWAkE8Ccd-BRjHEEgU" /><script src="$cdnUrl" integrity="$sriHash" crossorigin="anonymous"></script>
 "@
 
 # Write to output file
